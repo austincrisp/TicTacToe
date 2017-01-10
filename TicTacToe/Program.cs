ad37bbbd7d7b7db9610bdf6b47dae9b7075b2616
austincrisp <austi@DESKTOP-FL49QK5>
2017-01-10 15:38:53 -0500

    Initial push, small framework in place

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -10,6 +10,37 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
+            bool winner = false;
+            int counter = 0;
+
+            Console.WriteLine("Let's play Tic-Tac-Toe");
+            Console.WriteLine("Player One is X and Player Two is O");
+            Console.WriteLine("\n");
+
+            while (!winner)
+            {
+                Console.WriteLine("Turn: Player One");
+                string playerOne = Console.ReadLine();
+
+                string[] board = new string[]
+                {
+                "_","_","_","_","_","_","_","_","_"
+                };
+
+                foreach (string place in board)
+                {
+                    Console.Write(" " + place);
+                    if (counter == 2)
+                    {
+                        Console.WriteLine("");
+                        counter = 0;
+                    }
+                    else
+                    {
+                        counter++;
+                    }
+                }
+            }
         }
     }
 }
